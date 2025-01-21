5d45d7b35943521c848ff0a9ff2bd8cfe4b49467
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-21 12:42:25 +0100

    add: workflow

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // start code
+
+// workflow
+// 1. generare 5 numeri random
+//      - inserirli in un array
+// 2. far partire un timer di 30s
+// 3. far sparire i numeri generati a schermo
+// 4. appaiono input per far inserire all'utente i numeri che ricorda di aver visto
+// 5. controllo sui numeri indovinati
+//      - recupero i value degli input
+//      - controllo con ciclo se numeri inseriti dall'utente corrispondono a quelli random
+//      - se corrispondono:
+//              - aumento counter indovinati
+//              - inserire num indovinato in array num indovinati
+// 6. stampare risultato
